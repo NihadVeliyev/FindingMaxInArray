817b5f9fae3bfce7cde018a27b52f8caeccce631
xo.nihad <84568325+accept-your-fate@users.noreply.github.com>
2025-02-20 20:48:29 +0400

    salam qaqa

--- src/FindingMaxInArray/MaxInArray.java
+++ src/FindingMaxInArray/MaxInArray.java
@@ -5,24 +5,15 @@ import java.util.Scanner;
 public class MaxInArray {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        printArr(new int[]{1, 2, 3, 4});
 
-
-        System.out.print("Enter the number of elements: ");
-        int size=scanner.nextInt();
-        int [] numbers = new int[size];
-        System.out.print("Enter "+size+" elements: ");
-        for(int i=0;i<size;i++){
-            numbers[i]=scanner.nextInt();
-        }
-        int max= numbers[0];
-        for(int i=1;i<size;i++){
-            if(max< numbers[i]){
-                max= numbers[i];
-            }
-        }
-        System.out.println("Largest number is "+max);
-
-        scanner.close();
     }
+
+
+    public static void printArr(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("index: " + i + " value: " + arr[i]);
+        }
+    }
+
 }
